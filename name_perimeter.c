3f87dba6fd06c1dcc6fea8f8804c161427063041
Afrafarheen0205 <shifakbar23@gmail.com>
2024-09-30 15:29:09 +0530

    Create name_perimeter.c

---
+++ name_perimeter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  int l,b,area,peri;
+  scanf("%d%d",&l,&b);
+  area=l*b;
+  peri=2*(l+b);
+  printf("area: %d\n",area);
+  printf("perimeter: %d",peri);
+  return 0;
+  
+
+
+}
